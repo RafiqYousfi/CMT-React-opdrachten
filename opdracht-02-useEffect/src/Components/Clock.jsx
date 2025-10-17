82ff5554bbae702c4b48a4e9188448ae66231bad
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-10-17 12:12:09 +0200

    kj

---
+++ opdracht-02-useEffect/src/Components/Clock.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+ 
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+ 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+ 
+   
+    return () => clearInterval(intervalId);
+  }, []);
+ 
+  return <h2>{currentTime.toLocaleTimeString()}</h2>;
+};
+ 
+export default Clock;
