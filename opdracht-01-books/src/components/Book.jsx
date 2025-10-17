13d8d2e0438cd01fac430468ca12490abf453d04
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-10-17 09:54:53 +0200

    Update Book.jsx

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Book = ({image, title, author}) => {
+    const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+
+    function verhoogTeller() {
+        setAantalKeerGelezen(aantalKeerGelezen + 1)
+    }
     return (
-        <section className='book-container'>
+        <section className='book-container'> 
         <img className="image" src={image} alt="" />
         <h2>{title}</h2>
         <h3>{author}</h3>
+        <button onClick={verhoogTeller}>Keer gelezen: {aantalKeerGelezen}</button>
         </section>
     );
 };
