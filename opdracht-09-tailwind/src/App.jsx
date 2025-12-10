b70158fb237dbd2beec37b27294d820574b42063
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-12-10 11:39:00 +0100

    .

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
       <section className='mx-auto w-1/2'>
         <section className='w-48 '>
           <div className='flex'>
-            <img src="/img/foto2.png" alt="Workcation" />
+            <img src="./img/foto2.png" alt="Workcation" />
+     
             <h1>Work<span className='text-violet-300'>cation</span></h1>
           </div>
          <img className='w-64' src="/img/foto.png" alt="Workcation" />
