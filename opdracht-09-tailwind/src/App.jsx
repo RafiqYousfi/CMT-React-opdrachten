8e35ce20e219cf5e499c1b18600b6bc1b297f9a3
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-12-10 10:53:13 +0100

    .

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <h1>Work<span className='text-violet-300'>cation</span></h1>
           </div>
          <img className='w-64' src="/img/foto.png" alt="Workcation" />
-          <h2 className='text-2xl font-bold'>you can work form anywhere. <span className='text-violet-300'>take advantage of it</span></h2>
+          <h2 className='text-2xl font-bold'>you can work from anywhere. <span className='text-violet-300'>take advantage of it</span></h2>
           <p>workation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when your not on vacation</p>
 
           <button className='bg-violet-300'>Book your escape</button>
