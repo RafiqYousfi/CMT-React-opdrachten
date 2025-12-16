bae8bb9addcb14822f9230d933e8d1cdfe896ca6
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-12-16 13:37:57 +0100

    .

--- opdracht-01-books/src/data.js
+++ opdracht-01-books/src/data.js
@@ -4,18 +4,27 @@ const books = [
     author: "rafiq",
     image: "./images/book-1.png",
     category: "Avontuur",
+    description: "Een hilarisch verhaal over een jongen die constant in bizarre situaties terechtkomt. Hij probeert zijn schoolleven te overleven terwijl alles tegen lijkt te zitten. Een luchtig en grappig boek voor jong en oud.",
+    year: 2012,
+    pages: 224,
   },
   {
     title: "102 verdiepingen",
     author: "pieter",
     image: "./images/book-2.png",
     category: "Fantasy",
+    description: "In dit spannende fantasyboek beklimt een jonge held een toren met 102 verdiepingen vol mysterieuze uitdagingen. Elke verdieping brengt nieuwe gevaren en verrassingen. Een meeslepend avontuur dat je niet kunt neerleggen.",
+    year: 2018,
+    pages: 368,
   },
   {
     title: "10000 luchtballonen",
     author: "K3",
     image: "./images/book-3.png",
     category: "Romance",
+    description: "Een romantisch verhaal over liefde, vriendschap en onverwachte ontmoetingen. Tien duizend luchtballonnen symboliseren de dromen en verlangens van de hoofdpersonen. Perfect voor een gezellige leeservaring.",
+    year: 2020,
+    pages: 290,
   },
 ];
 
