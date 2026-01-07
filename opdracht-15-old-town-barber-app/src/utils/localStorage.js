bc639f1dcc827e68d8ca2bf58719e543d28abbf8
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2026-01-07 11:01:38 +0100

    .

---
+++ opdracht-15-old-town-barber-app/src/utils/localStorage.js
@@ -0,0 +1,21 @@
+export const setUserProfile = (profile) => {
+localStorage.setItem('userProfile', JSON.stringify(profile))
+}
+
+const getUserProfile = () => {
+   
+ try{
+    return JSON.parse(localStorage.getItem('userProfile')) || null
+   } catch {
+return null
+   }
+
+}
+
+const setAppointments = () => {
+    
+}
+
+const getAppointments = () => {
+    
+}
